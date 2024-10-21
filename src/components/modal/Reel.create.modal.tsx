4db55c343389b11edbf78d2bdbe2c9e4f1b9df40
Luc Pham <phamtienluc0601@gmail.com>
2024-10-21 18:24:10 +0700

    Create Reel successfully

--- src/components/modal/Reel.create.modal.tsx
+++ src/components/modal/Reel.create.modal.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
 import { FaUserFriends } from "react-icons/fa";
-import { FaEarthAmericas, FaLock } from "react-icons/fa6";
+import { FaEarthAmericas, FaLock, FaRegCircleCheck } from "react-icons/fa6";
 import { PiYoutubeLogoThin } from "react-icons/pi";
 import Select, { components, SingleValueProps } from 'react-select';
 import { uploadVideo } from "../../services/Entities/VideoService";
 import { ReelRequest } from "../../services/Types/Reel";
 import { createReel } from "../../services/Entities/ReelService";
+import avt from '../../assets/me1.jpg';
 
 interface IProps {
      show: boolean;
@@ -73,52 +74,69 @@ const CreateReelModal = (props: IProps) => {
 
      const [reel, setReel] = useState<string>("");
      const [openEditPanel, setOpenEditPanel] = useState<boolean>(false);
-
+     const [content, setContent] = useState('');
+     const [successMessage, setSuccessMessage] = useState('');
+     const [selectedFile, setSelectedFile] = useState<File | null>(null);
      const inputFile = useRef<HTMLInputElement | null>(null);
 
      const handleOpenFileBrowser = () => {
-          inputFile?.current?.click();
+          inputFile.current?.click();
      }
      const handleChooseFile = (event: React.ChangeEvent<HTMLInputElement>) => {
           if (event.target.files && event.target.files[0]) {
+               const file = event.target.files[0];
+               setSelectedFile(file);
                setReel(URL.createObjectURL(event.target.files[0]));
                setOpenEditPanel(true);
+          } else {
+               setReel("");
+               setSelectedFile(null);
+               setOpenEditPanel(false);
           }
      };
 
      const handleSaveFile = async () => {
-          if (inputFile.current && inputFile.current.files && inputFile.current.files[0]) {
-              try {
-                  // Upload video lên Cloudinary
-                  const uploadedData = await uploadVideo(inputFile.current.files[0]);
-                  console.log(uploadedData);
-                  console.log('Uploaded video URL:', uploadedData.secure_url);
-      
-                  // Tạo request để lưu vào database
-                  const reelRequest: ReelRequest = {
-                      userId: 3,
-                      url: uploadedData.secure_url,
-                      content: "Upload video testing", 
-                  };
-      
-                  // Gọi service để lưu reel vào database
-                  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhaGFoYWhhQGdtYWlsLmNvbSIsInBlcm1pc3Npb24iOlsiUk9MRV9VU0VSX0NSRUFURSIsIlJPTEVfVVNFUl9VUERBVEUiXSwiZXhwIjoxNzI5NTgzMDMzLCJpYXQiOjE3Mjk0OTY2MzMsInVzZXIiOnsiaWQiOjUsImVtYWlsIjoiYWhhaGFoYUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IkFETUlOIiwibG9jYXRpb24iOm51bGx9fQ.D0TyZsfS4-bSX40H64v5BwHcUCYxpTM-xlmn7GnEDz51mZc8CTi02sQzXPZQxWDwM5iHoZX7tfhwWqlLwmRyWA";
-                  const reelResponse = await createReel(reelRequest, token);
-                  console.log('Reel saved:', reelResponse);
-      
-                  // Đóng modal và reset trạng thái
-                  setOpenEditPanel(false);
-                  setShow(false);
-                  setReel("");
-              } catch (error) {
-                  console.error('Error during file upload or reel save:', error);
-              }
+          console.log('Selected file:', selectedFile);
+          if (selectedFile) {
+               try {
+                    // Upload video lên Cloudinary
+                    const uploadedData = await uploadVideo(selectedFile);
+                    console.log(uploadedData);
+                    console.log('Uploaded video URL:', uploadedData.secure_url);
+
+                    // Tạo request để lưu vào database
+                    const reelRequest: ReelRequest = {
+                         userId: 3,
+                         url: uploadedData.secure_url,
+                         content: content,
+                    };
+
+                    // Gọi service để lưu reel vào database
+                    const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhaGFoYWhhQGdtYWlsLmNvbSIsInBlcm1pc3Npb24iOlsiUk9MRV9VU0VSX0NSRUFURSIsIlJPTEVfVVNFUl9VUERBVEUiXSwiZXhwIjoxNzI5NTgzMDMzLCJpYXQiOjE3Mjk0OTY2MzMsInVzZXIiOnsiaWQiOjUsImVtYWlsIjoiYWhhaGFoYUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IkFETUlOIiwibG9jYXRpb24iOm51bGx9fQ.D0TyZsfS4-bSX40H64v5BwHcUCYxpTM-xlmn7GnEDz51mZc8CTi02sQzXPZQxWDwM5iHoZX7tfhwWqlLwmRyWA";
+                    const reelResponse = await createReel(reelRequest, token);
+                    console.log('Reel saved:', reelResponse);
+
+                    // Đóng modal và reset trạng thái
+                    setSuccessMessage('Reel đã được đăng tải thành công!');
+                    setOpenEditPanel(false);
+                    setReel("");
+               } catch (error) {
+                    console.error('Error during file upload or reel save:', error);
+                    alert('Có lỗi xảy ra khi đăng video. Vui lòng thử lại!');
+               }
+          } else {
+               alert('Vui lòng chọn một video để đăng.');
           }
-      };      
+     };
+
+     const handleChangeContent = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setContent(event.target.value);
+     };
 
      const handleClose = () => {
           setOpenEditPanel(false);
           setShow(false);
+          setSuccessMessage('');
           setReel("");
      }
 
@@ -140,7 +158,7 @@ const CreateReelModal = (props: IProps) => {
                                              Create New Reel
                                         </h3>
                                         <button
-                                             onClick={() => setShow(false)}
+                                             onClick={() => handleClose()}
                                              type="button"
                                              className="text-gray-400 bg-gray-100 text-9xl hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8"
                                         >
@@ -156,7 +174,7 @@ const CreateReelModal = (props: IProps) => {
                                         openEditPanel
                                              ? <div>
                                                   <div className="flex justify-start items-center gap-2 ml-5 mt-4">
-                                                       <img src="https://scontent.fhph1-1.fna.fbcdn.net/v/t39.30808-6/451418121_1493503281258736_1067539081919969742_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=_Zd01_0VGpYQ7kNvgGeWDHD&_nc_ht=scontent.fhph1-1.fna&oh=00_AYBHEAthlGeQj62ElU3i3-wL8Pg2Yne60mzoift-ZQxEKw&oe=66F439A4"
+                                                       <img src={avt}
                                                             className="rounded-[100%] text-base h-10 w-10 "
                                                             alt="error" />
                                                        <div className="">
@@ -176,7 +194,10 @@ const CreateReelModal = (props: IProps) => {
                                                        </div>
                                                   </div>
                                                   <div className="px-6 py-2 w-[100%] h-full">
-                                                       <input id="message" className="block border-none h-[20px] bg-transparent focus:outline-none py-3.5 w-full text-sm text-black rounded-lg " placeholder="Write your thoughts here..."></input>
+                                                       <input id="message" className="block border-none h-[20px] bg-transparent focus:outline-none py-3.5 w-full text-sm text-black rounded-lg "
+                                                            placeholder="Write your thoughts here..."
+                                                            value={content}
+                                                            onChange={handleChangeContent}></input>
                                                        <div className="relative border border-sky-600 mt-2 border-dashed h-[250px] flex justify-center">
                                                             <img
                                                                  className="object-contain w-[100%]"
@@ -186,7 +207,7 @@ const CreateReelModal = (props: IProps) => {
                                                             <button className="px-4 py-2 bg-amber-400 rounded-lg w-60 hover:bg-yellow-500 transition duration-200 text-white"
                                                                  onClick={() => handleOpenFileBrowser()}
                                                             >Select another</button>
-                                                            <input type='file' id='file'
+                                                            <input type='file' accept="video/*" id='file'
                                                                  onChange={handleChooseFile}
                                                                  ref={inputFile}
                                                                  style={{ display: 'none' }}
@@ -206,13 +227,19 @@ const CreateReelModal = (props: IProps) => {
                                                   >
                                                        Select from computer
                                                   </button>
-                                                  <input type='file' id='file'
+                                                  <input type='file' accept="video/*" id='file'
                                                        onChange={handleChooseFile}
                                                        ref={inputFile}
                                                        style={{ display: 'none' }}
                                                   />
                                              </div>
                                    }
+                                   {successMessage && (
+                                        <div className="flex flex-row self-center items-center justify-center gap-2 mt-2 p-2 bg-white text-2xl text-blue-400 rounded-lg">
+                                             <span>{successMessage}</span>
+                                             {successMessage? <FaRegCircleCheck /> : <></>}
+                                        </div>
+                                   )}
                               </div>
                          </div>
                     </div>
