af5ef67b32529a5495ccf50a806920f1f2da3335
Luc Pham <phamtienluc0601@gmail.com>
2024-11-04 22:18:44 +0700

    Add User Card placeholders

--- src/components/user/UserChatCard.tsx
+++ src/components/user/UserChatCard.tsx
@@ -10,6 +10,23 @@ interface UserCardProps {
 
 const UserChatCard: React.FC<UserCardProps> = ({ name, avatar, lastChat, lastChatSince }) => {
 
+    const placeholder = (
+        <div className="flex w-[98%] ms-1 rounded-lg justify-between overflow-hidden animate-pulse bg-gray-100 cursor-pointer">
+            <div className="flex items-start relative p-2">
+                <div className="w-16 h-16 bg-gray-300 rounded-full"></div>
+                <div className="absolute bottom-2 left-12 w-5 h-5 bg-gray-300 rounded-full"></div>
+
+                <div className="ml-4 flex flex-col items-start">
+                    <div className="h-5 bg-gray-300 rounded w-3/4 mb-2"></div>
+                    <div className="flex gap-2 w-full">
+                        <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+                        <div className="h-4 bg-gray-300 rounded w-1/4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <div>
             <div className="flex w-[98%] ms-1 rounded-lg justify-between overflow-hidden cursor-pointer hover:bg-[#F1F1F1]">
@@ -19,7 +36,7 @@ const UserChatCard: React.FC<UserCardProps> = ({ name, avatar, lastChat, lastCha
                         src={avatar}
                         alt={`${name}'s avatar`}
                     />
-                    <img className="absolute bottom-2 left-12 w-5 h-5 rounded-full object-cover" 
+                    <img className="absolute bottom-2 left-12 w-5 h-5 rounded-full object-cover"
                         src="../src/assets/img/icons/onlineIcon.png"></img>
                     <div className="ml-4 flex flex-col items-start">
                         <h2 className="text-lg font-bold text-gray-800">{name}</h2>
--- src/components/user/UserSimpleCard.tsx
+++ src/components/user/UserSimpleCard.tsx
@@ -7,6 +7,17 @@ interface UserSimpleCardProps {
 }
 
 const UserSimpleCard: React.FC<UserSimpleCardProps> = ({ userFullName, avatarUrl }) => {
+
+	const placeholder = (
+		<div className="flex w-full p-1 justify-between overflow-hidden rounded-lg animate-pulse bg-gray-100 cursor-pointer">
+            <div className="flex items-center relative w-full gap-4 py-1">
+                <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                <div className="absolute bottom-1 left-7 w-3 h-3 bg-gray-300 rounded-full"></div>
+                <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+            </div>
+        </div>
+	);
+
 	return (
 		<div className="flex w-full p-1 justify-between overflow-hidden rounded-lg cursor-pointer hover:bg-[#E7E7E7]">
 			<div className="flex items-center relative w-full gap-4 py-1">
