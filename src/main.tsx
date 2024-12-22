c510f8925b7134960e819672afef5065a9b552d3
Anh Linh <linhnguyenanh75@gmail.com>
2024-12-22 16:26:31 +0700

    config redux-persist to stay logged in after reloading

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/index.scss'
-import { store } from './redux/store.ts'
+import { persistor, store } from './redux/store.ts'
 import { Provider } from 'react-redux'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../redux/features/counter/counterSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Lưu trữ trong localStorage
+import counterReducer from '../redux/features/counter/counterSlice';
 import accountReducer from '../redux/slice/accountSlice';
 
+// Cấu hình redux-persist cho accountReducer
+const accountPersistConfig = {
+  key: 'account', // Khóa để lưu trữ state này
+  storage,
+};
+
+const persistedAccountReducer = persistReducer(accountPersistConfig, accountReducer);
+
 export const store = configureStore({
-     reducer: {
-          counter: counterReducer,
-          account: accountReducer,
-     },
-})
+  reducer: {
+    counter: counterReducer, // Reducer không cần lưu trữ
+    account: persistedAccountReducer, // Reducer với redux-persist
+  },
+});
+
+// Thêm persistor để đồng bộ
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
